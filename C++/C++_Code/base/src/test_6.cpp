031a12f000f92e0a00eff0e4556be0425969498d
wdl2019916 <wdl20190312@gmail.com>
2020-05-10 17:13:07 +0800

    函数指针补充

--- C++/C++_Code/base/src/test_6.cpp
+++ C++/C++_Code/base/src/test_6.cpp
@@ -1,32 +1,65 @@
+/*关于类的成员函数指针的使用*/
+
 #include<iostream>
 #include<cmath>
 using namespace std;
 
-double Getln2(int n)
+class CA;
+typedef int (CA::*pFun)(int,int);  
+
+class CA
 {
-	int i;
-	double sum = 0;
-	for (i = 1; i <= n; i++)
+	public:
+	int Max(int a,int b)
 	{
-		double j = 1.0 / i * pow(-1, i - 1);
-		sum = sum + j;
+		return a > b ? a:b;
 	}
-	return sum;
+
+	int Min(int a,int b)
+	{
+		return a < b? a:b;
+	}
+
+	static int sMax(int a,int b)
+	{
+		return a < b?a :b;
+	}
+
+	static int Sum(int a,int b)
+	{
+		return a+b;
+	}
+
+	int Result(pFun fun,int a,int b)
+	{
+		return (this->*fun)(a,b);
+	}
+};
+
+int Result(CA* pA,pFun fun,int a,int b)
+{
+	return (pA->*fun)(a,b);
 }
+
 
 int main()
 {
 	int n = 10;
-	double ln2 = 0.69314718;
-	double R = Getln2(10);
-	while (fabs(ln2 - R) > 2.0e-5)
-	{
-		n++;
-		R = Getln2(n);
-	}
+    
+    CA ca;
 
-	cout << "根据泰勒展开式（麦克劳林展开式），由前" << n << "项可得" << endl;
-	cout << "ln2 = " << Getln2(n) << endl;
+	int a = 3;
+	int b = 4;
+
+	cout << "1:" << ca.Result(CA::Max,a,b) << endl;
+	cout << "2:" << Result(&ca,CA::Max,a,b) << endl;
 
 	return 0;
 }
+
+/*
+总结：
+1.我们声明类的成员函数指针必须要确保：成员函数指针必须包含类名，因此类必须已经声明
+2.因为成员函数指针必须依赖于类的实例化存在，所以这里这也决定了在该类中调用及类外调用该成员函数指针存在不同：该类中调用，实际已经存在了this，所以不需要传入类的实例化，我们把this当做这个实例化使用，而类外，则必须传入一个类的实例化对象；但是如果是一个静态成员函数呢？那么即使是类外，我们也是不需要传入实例化对象的；此时等同于普通函数；	
+3.为什么必须要是用pA->*fun而不是pA->fun呢？如果我们把函数也作为类的成员，那么该类拥有的其实是函数对象，而非其其指针，所有我们需要解引用；
+*/
