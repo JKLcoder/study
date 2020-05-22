eeccd3c75dd61cb15260c99b384613927afacdd9
wdl2019916 <wdl20190312@gmail.com>
2020-05-22 23:10:39 +0800

    读写文件

--- C++/C++_Code/base/src/test_8.cpp
+++ C++/C++_Code/base/src/test_8.cpp
@@ -1,3 +1,6 @@
+/*
+关于写入文件
+*/
 #include <fstream>
 #include <iostream>
 
@@ -12,7 +15,7 @@ int main()
 
     ofstream outFile;
     outFile.open("carinfo.txt");
-    if (!outFile.is_open())     
+    if (!outFile.is_open())
         return 0;
 
     cout << "Enter the make and model of automobile:";
@@ -40,8 +43,43 @@ int main()
     outFile.setf(ios_base::showpoint); //强制显示小数点输出
     outFile << "Make and Model:" << automobile << endl;
     outFile << "Year:" << year << endl;
-    outFile << "Was asking" << a_price << endl;
+    outFile << "Was asking:" << a_price << endl;
 
     outFile.close();
+
+    //read data from file
+    ifstream inFile("carinfo.txt");
+    if (!inFile.is_open())
+        return 0;
+
+    string str;
+    cout << "read data from file:" << endl;
+
+    while (getline(inFile, str))
+        // while (inFile >> str)
+        cout << str << endl;
+
+    if (inFile.eof())
+        cout << "End of file reached.\n";
+    else if (inFile.fail())
+        cout << "Input terminated by data mismatch.\n";
+    else
+        cout << "Input terminated for unknown reason.\n";
+
     return 0;
 }
+
+/*
+总结：
+ 1.这里面提供了两种文件打开方式，一种是使用outFile.open("carinfo.txt");另一种是ifstream inFile("carinfo.txt");后者在进行类构造时会默认打开文件；
+ 2.从文件中读取时可以使用两种方式，getline(inFile, str)；inFile >> str；前者是一行一行读取，以换行符作为结束符，后者以空格作为结束符，如果要一行一行读取，只能使用getline；
+ 3.使用is_open判断文件是否打开；
+ 4.读取文件时什么状况会返回inFile.fail()呢？假设上面从文件中读取文件时，str时int类型，那么此时就会出现不匹配，从而出现此错误；
+ 5.考虑一下，为什么while (inFile >> str)可以用于判断文件读取流是否成功呢？结合源码如下：
+   operator void *() const {
+        if(state&(badbit|failbit) )
+            return 0;
+        return (void* )this;
+    }
+   当我们读取从文件中读取时，stream.iostate状态一旦出现badbit或failbit，那么就会返回空，也就是false状态；
+*/
